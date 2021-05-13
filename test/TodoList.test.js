1e20baa0527a4d3e2d443618e1d14b2b79cc4648
DESKTOP-MRO5CVV\kaiti <kaitie.98@hotmail.com>
2021-05-13 11:54:27 +1000

    Add test

---
+++ test/TodoList.test.js
@@ -0,0 +1,27 @@
+const { assert } = require("chai");
+
+const TodoList = artifacts.require('./TodoList.sol');
+
+contract('TodoList', (accounts) => {
+    before(async () => {
+        this.TodoList = await TodoList.deployed();
+    });
+
+    it('deploys successfully', async () => {
+        const address = await this.TodoList.address;
+        assert.notEqual(address, 0x0);
+        assert.notEqual(address, '');
+        assert.notEqual(address, null);
+        assert.notEqual(address, undefined);
+    });
+
+    it('list tasks', async () => {
+        const taskCount = await this.TodoList.taskCount();
+        const task = await this.TodoList.tasks(taskCount);
+        assert.equal(task.id.toNumber(), taskCount.toNumber());
+        assert.equal(task.content, 'My first task');
+        assert.equal(task.completed, false);
+        assert.equal(taskCount.toNumber(), 1);
+    });
+
+});
