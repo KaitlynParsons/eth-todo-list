0a3d99584d98944d7aef2bdaa595788f879c1c32
DESKTOP-MRO5CVV\kaiti <kaitie.98@hotmail.com>
2021-05-13 12:26:17 +1000

    Toggle completed

--- src/app.js
+++ src/app.js
@@ -64,12 +64,12 @@ App = {
   },
 
   render: async () => {
-     // Update app loading state
+    // Update app loading state
     App.setLoading(true);
 
     // Render Account
     $("#account").html(App.account);
-    
+
     // Render Tasks
     await App.renderTasks();
 
@@ -80,45 +80,55 @@ App = {
   renderTasks: async () => {
     // Load the total task count from the blockchain
     const taskCount = await App.todoList.taskCount();
-    const $taskTemplate = $('.taskTemplate');
+    const $taskTemplate = $(".taskTemplate");
+
     // Render out each task with a new task template
     for (let i = 1; i <= taskCount; i++) {
-        const task = await App.todoList.tasks(i);
-        const taskId = task[0].toNumber();
-        const taskContent = task[1];
-        const taskCompleted = task[2];
+      // Fetch the task data from the blockchain
+      const task = await App.todoList.tasks(i);
+      const taskId = task[0].toNumber();
+      const taskContent = task[1];
+      const taskCompleted = task[2];
 
-        // Create the html for the task
-        const $newTaskTemplate = $taskTemplate.clone();
-        $newTaskTemplate.find(".content").html(taskContent);
-        $newTaskTemplate.find(".input")
-          .prop("name", taskId)
-          .prop("checked", taskCompleted)
-        //   .on("click", App.toggleCompleted);
+      // Create the html for the task
+      const $newTaskTemplate = $taskTemplate.clone();
+      $newTaskTemplate.find(".content").html(taskContent);
+      $newTaskTemplate
+        .find("input")
+        .prop("name", taskId)
+        .prop("checked", taskCompleted)
+        .on("click", App.toggleCompleted);
 
-        // Put the task in the correct list
-        if (taskCompleted) {
-            $('#completedTaskList').append($newTaskTemplate);
-        } else {
-            $('#taskList').append($newTaskTemplate);
-        }
-        // Show the task
-        $newTaskTemplate.show();
+      // Put the task in the correct list
+      if (taskCompleted) {
+        $("#completedTaskList").append($newTaskTemplate);
+      } else {
+        $("#taskList").append($newTaskTemplate);
+      }
+
+      // Show the task
+      $newTaskTemplate.show();
     }
-
   },
 
   createTask: async () => {
     App.setLoading(true);
-    const content = $('#newTask').val();
+    const content = $("#newTask").val();
     await App.todoList.createTask(content);
+    window.location.reload();
+  },
+
+  toggleCompleted: async (e) => {
+    App.setLoading(true);
+    const taskId = e.target.name;
+    await App.todoList.toggleCompleted(taskId);
     window.location.reload();
   },
 
   setLoading: (boolean) => {
     App.loading = boolean;
-    const loader = $('#loader');
-    const content = $('#content');
+    const loader = $("#loader");
+    const content = $("#content");
     if (boolean) {
       loader.show();
       content.hide();
